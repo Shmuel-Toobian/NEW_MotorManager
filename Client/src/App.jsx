fc979dd0a15a601715baf572c64158853c41463a
Shmuel-Toobian <shmueltoobian@gmail.com>
2024-12-26 11:24:56 +0200

    updateed Map and navigate to map with new locatiom

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,10 +6,11 @@ import { AuthProvider } from "./store/useContext";
 import Footer from "./components/footer/Footer";
 import Home from "./Pages/Home/Home";
 import SignUp from "./components/signUp/SignUp"
-import Cars from "./components/Cars/Cars";
+import Cars from "./components/cars/Cars";
 import Header from "./components/header/Header";
 import Login from "./components/login/Login";
 import AddCar from "./components/cars/AddCar";
+import Map from "./components/map/Map";
 
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/cars" element={<Cars />} />
           <Route path="/addcar" element={<AddCar />} />
+          <Route path="/map" element={<Map />} />
         </Routes>
         <Footer/>
       </Router>
